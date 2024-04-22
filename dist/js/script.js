ee70995c320faea32452c3fe68d17dce73950a42
unknown <neet693@gmail.com>
2024-04-22 15:23:24 +0700

    push portfolio tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,48 @@
+//Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector(`header`);
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+//Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//Mobile Menu Anywhere
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+//  npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
